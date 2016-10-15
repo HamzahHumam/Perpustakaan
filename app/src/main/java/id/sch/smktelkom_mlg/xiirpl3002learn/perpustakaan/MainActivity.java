1e5a46bba8f1e43baba0e0a43763ee76d7c5ad84
gatonsugara <agatonkahfi@gmail.com>
2016-10-15 21:48:58 +0700

    Tahap radiobutton dan checkbox

--- app/src/main/java/id/sch/smktelkom_mlg/xiirpl3002learn/perpustakaan/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/xiirpl3002learn/perpustakaan/MainActivity.java
@@ -4,13 +4,18 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     TextView tvHasil, tvHasil2, tvhasil3;
     EditText etinput;
     Button btOk;
+    CheckBox cb1, cb2, cb3;
+    RadioGroup rg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,8 +24,12 @@ public class MainActivity extends AppCompatActivity {
         tvHasil = (TextView) findViewById(R.id.textViewHasil);
         tvHasil2 = (TextView) findViewById(R.id.textView2);
         tvhasil3 = (TextView) findViewById(R.id.textView3);
+        rg = (RadioGroup) findViewById(R.id.rgpilihan);
         etinput = (EditText) findViewById(R.id.editTextinput);
         btOk = (Button) findViewById(R.id.buttonOK);
+        cb1 = (CheckBox) findViewById(R.id.checkBoxbk);
+        cb2 = (CheckBox) findViewById(R.id.checkBoxkm);
+        cb3 = (CheckBox) findViewById(R.id.checkBoxmj);
 
         btOk.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,8 +41,29 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void goHasil() {
+        String hasil = "Tidak ada Masukan";
+        if (rg.getCheckedRadioButtonId() != -1) {
+            RadioButton rB = (RadioButton)
+                    findViewById(rg.getCheckedRadioButtonId());
+            hasil = rB.getText().toString();
+        }
+        if (hasil == null) {
+            tvHasil.setText("Belum memilih ");
+        } else {
+            tvHasil.setText("Masukkan pilihan Anda : " + hasil);
+        }
+
+        String hasil2 = " \n\nTambah Masa Pinjam selama : \n";
+        int startlen = hasil2.length();
+        if (cb1.isChecked()) hasil2 += cb1.getText() + "\n";
+        if (cb2.isChecked()) hasil2 += cb2.getText() + "\n";
+        if (cb3.isChecked()) hasil2 += cb3.getText() + "\n";
+
+        if (hasil2.length() == startlen) hasil2 += " Tidak ada ";
+
         if (isValid()) ;
         String input = etinput.getText().toString();
+        tvHasil.setText("Layanan Buku : " + hasil + " " + hasil2);
         tvhasil3.setText("Komentar dan saran : " + input);
     }
 
